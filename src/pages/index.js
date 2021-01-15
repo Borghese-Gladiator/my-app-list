c95d6863c6ad774e65d5f25328a3131f0dc014ab
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2021-01-15 04:44:04 -0500

    render list of cards

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,7 @@
-import * as React from "react"
+import * as React from "react";
+import {
+  TaggedContentCard
+} from 'react-ui-cards';
 
 // styles
 const pageStyles = {
@@ -7,15 +10,10 @@ const pageStyles = {
   fontFamily: "-apple-system, Roboto, sans-serif, serif",
 }
 const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
+  marginTop: 0
 }
 const headingAccentStyles = {
   color: "#663399",
-}
-const paragraphStyles = {
-  marginBottom: 48,
 }
 const codeStyles = {
   color: "#8A6534",
@@ -52,12 +50,30 @@ const descriptionStyle = {
   fontSize: "14px",
 }
 
+// custom styles
+const rootStyles = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+}
+
+const cardContainerStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  flexDirection: 'row',
+  flexWrap: 'wrap'
+}
+
+
+// data
 const docLink = {
   text: "Documentation",
   url: "https://www.gatsbyjs.com/docs/",
   color: "#8954A8",
 }
-// data
+
 const links = [
   {
     text: "Tutorial",
@@ -96,49 +112,81 @@ const links = [
   },
 ]
 
+const appList = [
+  {
+    link: 'https://github.com/nukeop',
+    thumbnail: 'https://i.imgur.com/yCch7GN.jpg',
+    title: 'Cheesecake',
+    date: '15 January 2021',
+    tags: [
+      'food',
+      'cake',
+      'fruits'
+    ]
+  },
+  {
+    link: 'https://github.com/nukeop',
+    thumbnail: 'https://i.imgur.com/yCch7GN.jpg',
+    title: 'Cheesecake',
+    date: '15 January 2021',
+    tags: [
+      'food',
+      'cake',
+      'fruits'
+    ]
+  },
+  {
+    link: 'https://github.com/nukeop',
+    thumbnail: 'https://i.imgur.com/yCch7GN.jpg',
+    title: 'Cheesecake',
+    date: '15 January 2021',
+    tags: [
+      'food',
+      'cake',
+      'fruits'
+    ]
+  }
+]
+
 // markup
 const IndexPage = () => {
   return (
     <main style={pageStyles}>
       <title>Home Page</title>
-      <h1 style={headingStyles}>
-        Congratulations
-        <br />
-        <span style={headingAccentStyles}>— you just made a Gatsby site! </span>
-        <span role="img" aria-label="Party popper emojis">
-          🎉🎉🎉
-        </span>
-      </h1>
-      <p style={paragraphStyles}>
-        Edit <code style={codeStyles}>src/pages/index.js</code> to see this page
-        update in real-time.{" "}
-        <span role="img" aria-label="Sunglasses smiley emoji">
-          😎
-        </span>
-      </p>
-      <ul style={listStyles}>
-        <li style={docLinkStyle}>
-          <a
-            style={linkStyle}
-            href={`${docLink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-          >
-            {docLink.text}
-          </a>
-        </li>
-        {links.map(link => (
-          <li style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              <p style={descriptionStyle}>{link.description}</p>
-            </span>
-          </li>
-        ))}
-      </ul>
+      <div style={rootStyles}>
+        <h1 style={headingStyles}>
+          <span>Borghese-Gladiator{" "}<span style={headingAccentStyles}>App List</span></span>
+          <span role="img" aria-label="Party popper emojis">
+            🎉🎉🎉
+          </span>
+        </h1>
+        <p>
+          <span>Track apps I made using tutorials </span>
+          <span role="img" aria-label="Sunglasses smiley emoji">
+            😎
+          </span>
+          <code style={codeStyles}>Made with GatsbyJS</code>
+        </p>
+        <p>
+          <span>Click to see deployed app</span>
+        </p>
+      </div>
+      <div style={cardContainerStyle}>
+        {
+          appList.map((appObj) => {
+            const { link, thumbnail, title, date, tags } = appObj;
+            return (
+              <TaggedContentCard
+                href={link}
+                thumbnail={thumbnail}
+                title={title}
+                description={date}
+                tags={tags}
+              />
+            );
+          })
+        }
+      </div>
       <img
         alt="Gatsby G Logo"
         src="data:image/svg+xml,%3Csvg width='24' height='24' fill='none' xmlns='http://www.w3.org/2000/svg'%3E%3Cpath d='M12 2a10 10 0 110 20 10 10 0 010-20zm0 2c-3.73 0-6.86 2.55-7.75 6L14 19.75c3.45-.89 6-4.02 6-7.75h-5.25v1.5h3.45a6.37 6.37 0 01-3.89 4.44L6.06 9.69C7 7.31 9.3 5.63 12 5.63c2.13 0 4 1.04 5.18 2.65l1.23-1.06A7.959 7.959 0 0012 4zm-8 8a8 8 0 008 8c.04 0 .09 0-8-8z' fill='%23639'/%3E%3C/svg%3E"
