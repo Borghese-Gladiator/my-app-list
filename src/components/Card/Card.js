f2cb8bcad5057931e297870eefb51236d8ee24be
Winged Victory of Samothrace <timothy_shee@hotmail.com>
2022-04-08 16:39:03 -0400

    Ignore keys if null

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -16,7 +16,11 @@ const Card = ({
     <img src={thumbnail} alt="thumbnail" />
     <div className="card-body">
       <h2>{title}</h2>
-      <h4><a href={github_link} target="_blank" rel="noopener noreferrer">{github_link}</a></h4>
+      {
+        github_link == null
+          ? <></>
+          : <h4><a href={github_link} target="_blank" rel="noopener noreferrer">{github_link}</a></h4>
+      }
       <p>{description}</p>
       {
         keywords.map((item, idx) => <span key={`demo_snap_${idx}`} className="chip">{item}</span>)
@@ -26,7 +30,11 @@ const Card = ({
           <h4>{dateCreated}</h4>
           <span id="label-last-updated">{`Last Updated: ${dateLastUpdated}`}</span>
         </div>
-        <a href={link} target="_blank" rel="noopener noreferrer" className="card-link-btn">Demo</a>
+        {
+          link == null
+            ? <></>
+            : <a href={link} target="_blank" rel="noopener noreferrer" className="card-link-btn">Demo</a>
+        }
       </div>
     </div>
   </div>
